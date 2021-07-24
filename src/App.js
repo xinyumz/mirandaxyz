a983d213405a779cc8b1addc354dad3c076df4fb
Miranda Zhang <xinyumz@umich.edu>
2021-07-24 19:13:03 -0400

    update_0724

--- src/App.js
+++ src/App.js
@@ -16,6 +16,111 @@ import Kiwi from "./images/Kiwi.jpg";
 import CarTower from "./images/CameroonTower.jpg";
 import Meow from "./images/Meow.jpg";
 
+let exampleObj = [
+  {
+    institution: "",
+    location: "",
+    departments: [
+      {
+        title: "",
+        dates: [],
+        positions: [{ title: "", date: "" }],
+        supervisor: "",
+        resumeItems: [],
+      },
+    ],
+  },
+];
+
+let experienceItems = [
+  {
+    institution: "University of Michigan, ",
+    location: "Ann Arbor, MI",
+    departments: [
+      {
+        title: "Laine Lab",
+        dates: ["Sep. 2016 - Jun. 2021"],
+        positions: [
+          {
+            title: "Lab safety manager",
+            date: "Sep. 2016 - Jun. 2021",
+          },
+          {
+            title: "Graduate student research assistant (GSRA)",
+            date: "Sep. 2017 - Jun. 2021",
+          },
+        ],
+        supervisor: "Supervisor: Prof. Richard M. Laine",
+        resumeItems: [
+          {
+            title: "<span>Li(Na)<sub>x</sub>SiON Polymer Precursors</span>",
+            date: "Jan. 2020 - Jun. 2021",
+            descs: [
+              "Syntheses and characterization of Li<sub>x</sub>SiON and Na<sub>x</sub>SiON polymer precursors as solid electrolytes derived from agricultural waste, rice hull ash, towards all solid-state batteries (ASSBs).",
+            ],
+          },
+          {
+            title: "<span>Li<sub>x</sub>PON-like Polymer Precursors</span>",
+            date: "Sep. 2017 - Jun. 2021",
+            descs: [
+              "Syntheses and characterization of Li<sub>x</sub>PON-like polymer precursors as solid electrolytes towards ASSBs. Precursor systems include Li<sub>x</sub>PON, Li<sub>x</sub>SiPON and Li<sub>x</sub>SiPHN.",
+            ],
+          },
+          {
+            title: "<span>Oxysilylation of Diepoxides</span>",
+            date: "Aug. 2017 - Dec. 2019",
+            descs: [
+              "Syntheses and characterization of hybrid materials with nano-sized struc-tures and their derivatives, a novel approach towards epoxy resins.",
+            ],
+          },
+          {
+            title: "<span>Toughened Alumina (ZTA) Films and Coatings</span>",
+            date: "Aug. 2018 - Feb. 2019",
+            descs: [
+              "Sintering of ZTA thin (~40  &#x3BCm) and thick (~200  &#x3BCm) films from nanopow-erders by liquid-feed flame spray pyrolysis; coating of ZTA thin films (< 5  &#x3BCm) on Si3N4 (~300  &#x3BCm) and AlN (~600  &#x3BCm) substrates as a protective reinforce-ment layer for power electronic applications.",
+            ],
+          },
+        ],
+      },
+      {
+        title: "Graduate student instructor (GSI)",
+        dates: ["Sep. 2020 - Apr. 2021", "Sep. 2018 - Dec. 2018"],
+        positions: [{ title: "", date: "" }],
+        supervisor: "",
+        resumeItems: [],
+      },
+    ],
+  },
+  {
+    institution: "Harbin Institute of Technology, ",
+    location: "Harbin, China",
+    departments: [
+      {
+        title: "Undergraduate research assistant",
+        dates: ["Jul. 2015 - Jun. 2016"],
+        positions: [],
+        supervisor: "Supervisor: Prof. Yujin Wang",
+        resumeItems: [
+          {
+            title: "<span>Eutectic MeB2-44MeC (Me = Ti, Zr) Composites</span>",
+            date: "Sep. 2015 - Jun. 2016",
+            descs: [
+              "Sintering (hot-pressing) of ceramics from ball milled mi-cron/submicron-size powders",
+            ],
+          },
+          {
+            title: "<span>B4C Multiphase Materials</span>",
+            date: "Jul. 2015 - Aug. 2015",
+            descs: [
+              "Precursor syntheses, sintering and characterization of B4C ceramics.",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
 function App() {
   return (
     <div>
@@ -249,40 +354,53 @@ class ResumeItem extends React.Component {
   }
 }
 
+class Department extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 class="li-title container">{this.props.department.title}</h3>
+        <div class="date">
+          {this.props.department.dates.map((date, index) => (
+            <div class="each-date" key={index}>
+              {date}
+            </div>
+          ))}
+        </div>
+        <div>
+          {" "}
+          <em>{this.props.department.supervisor}</em>
+        </div>
+        {this.props.department.resumeItems.map((item, index) => (
+          <ResumeItem key={index} item={item} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class Experience extends React.Component {
+  render() {
+    return (
+      <div class="list">
+        <h3 class="li-title container">
+          <span>
+            {this.props.experience.institution}
+            <span class="nonbold">{this.props.experience.location}</span>
+          </span>
+        </h3>
+        {this.props.experience.departments.map((department, index) => (
+          <Department key={index} department={department} />
+        ))}
+      </div>
+    );
+  }
+}
+
 class CV extends React.Component {
   constructor() {
     super();
     this.state = {
-      resumeItems: [
-        {
-          title: "<span>Li(Na)<sub>x</sub>SiON Polymer Precursors</span>",
-          date: "Jan. 2020 - Jun. 2021",
-          descs: [
-            "Syntheses and characterization of Li<sub>x</sub>SiON and Na<sub>x</sub>SiON polymer precursors as solid electrolytes derived from agricultural waste, rice hull ash, towards all solid-state batteries (ASSBs).",
-          ],
-        },
-        {
-          title: "<span>Li<sub>x</sub>PON-like Polymer Precursors</span>",
-          date: "Sep. 2017 - Jun. 2021",
-          descs: [
-            "Syntheses and characterization of Li<sub>x</sub>PON-like polymer precursors as solid electrolytes towards ASSBs. Precursor systems include Li<sub>x</sub>PON, Li<sub>x</sub>SiPON and Li<sub>x</sub>SiPHN.",
-          ],
-        },
-        {
-          title: "<span>Oxysilylation of Diepoxides</span>",
-          date: "Aug. 2017 - Dec. 2019",
-          descs: [
-            "Syntheses and characterization of hybrid materials with nano-sized struc-tures and their derivatives, a novel approach towards epoxy resins.",
-          ],
-        },
-        {
-          title: "<span>Toughened Alumina (ZTA) Films and Coatings</span>",
-          date: "Aug. 2018 - Feb. 2019",
-          descs: [
-            "Sintering of ZTA thin (~40  &#x3BCm) and thick (~200  &#x3BCm) films from nanopow-erders by liquid-feed flame spray pyrolysis; coating of ZTA thin films (< 5  &#x3BCm) on Si3N4 (~300  &#x3BCm) and AlN (~600  &#x3BCm) substrates as a protective reinforce-ment layer for power electronic applications.",
-          ],
-        },
-      ],
+      experienceItems,
     };
   }
 
@@ -357,32 +475,14 @@ class CV extends React.Component {
                 </div>
               </div>
             </div>
+
             <div class="sec">
               <h2 class="sec-title">Experience</h2>
-
-              <div class="list">
-                <h3 class="li-title container">
-                  <span>
-                    University of Michigan,{" "}
-                    <span class="nonbold">Ann Arbor, MI</span>
-                  </span>
-                </h3>
-                <h3 class="li-title container">
-                  {" "}
-                  <span>Laine lab</span>
-                  <span>Sep. 2016 - Jun.2021</span>
-                </h3>
-                <div>
-                  {" "}
-                  <em>
-                    Supervisor: Prof. Richard M. Laine (University of Michigan)
-                  </em>
-                </div>
-              </div>
-              {this.state.resumeItems.map((item, index) => (
-                <ResumeItem key={index} item={item} />
+              {this.state.experienceItems.map((experience, index) => (
+                <Experience key={index} experience={experience} />
               ))}
             </div>
+
             <div class="sec">
               <h2 class="sec-title">Intership</h2>
               <div class="list">
